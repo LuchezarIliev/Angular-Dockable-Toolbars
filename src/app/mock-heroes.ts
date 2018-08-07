8f610e899f438255ab91eadd34844a0f6fd48087
Georgi Danchev <Georgi Danchev@nemetschek.bg>
2018-08-07 11:35:27 +0300

    removed heroes mock

--- src/app/mock-heroes.ts
+++
@@ -1,7 +0,0 @@
-import { Hero } from './hero';
-
-export const HEROES: Hero[] = [
-    { id: 123, name: 'Luchezar Iliev' },
-    { id: 234, name: 'Kostadin Krushkov' },
-    { id: 345, name: 'Martin Ivanov' }
-];
