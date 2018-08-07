939dfc057b26dc8ccf50cb6a199da47a1c4f0619
Lachezar Iliev <Lachezar Iliev@nemetschek.bg>
2018-08-07 09:37:39 +0300

    Added loading from Settings

--- src/app/master-view.component.ts
+++ src/app/master-view.component.ts
@@ -22,16 +22,26 @@ export class AppComponent implements AfterContentInit, OnInit {
       this.toolbarComponent.showToolbarComponent(visibility);
     }
 
-    public onleavepage(): void {
+    public persistCurrentState(): void {
+
       if(this.dsxdfUtil != null) {
         this.dsxdfUtil.saveStatesIntoKey("Settings");
+        console.log("State Saved!");
       }
+      
     }
 
+    /* After the content is initiallized: 
+     > create the central component (to which the toolbars will be docked);
+     > make it fixed to the center;
+     > load persisted state from 'Settings';
+    */
     ngAfterContentInit() {
+
       this.dsxdfUtil = DSXDFUtil.createDSXDFUtil();
-      this.dsxdfUtil.loadStatesFromKey("Settings");
       this.dsxdfUtil.addFixedPanel(document.getElementById("centerdiv"), DSXDFUtil.fixedCenter);
+      this.dsxdfUtil.loadStatesFromKey("Settings");
+      
     }
 
     ngOnInit() {
--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -13,6 +13,12 @@ declare var DSXDFPanel: DockPosition;
 
 export class ToolbarComponent implements AfterContentInit, OnInit {
 
+    public mainView: ToolbarPanel;
+    public toolbar1: ToolbarPanel;
+    public toolbar2: ToolbarPanel;
+    public toolbar3: ToolbarPanel;
+    public toolbar4: ToolbarPanel;
+
     private toolbarInit(
       mainView: ToolbarPanel,
       tbTitle: string,
@@ -27,6 +33,7 @@ export class ToolbarComponent implements AfterContentInit, OnInit {
       toolbar.initLayout(300, 50, tbWidth, tbHeigth, tbPosition);
 
       return toolbar;
+      
     }
 
     public showToolbarComponent(visibility): void {
@@ -35,41 +42,13 @@ export class ToolbarComponent implements AfterContentInit, OnInit {
 
     public ngAfterContentInit(): void {
 
-        let mainView: ToolbarPanel = DSXDFUtil.createDSXDFUtil();
-        //mainView.addFixedPanel(document.getElementById("centerdiv"), DSXDFUtil.fixedCenter);
+        this.mainView = DSXDFUtil.createDSXDFUtil();
 
-        let toolbar1: ToolbarPanel = this.toolbarInit(mainView, "Toolbar #1", "mydfa", "toolbar1", 400, 100, DSXDFPanel.dockTop),
-            toolbar2: ToolbarPanel = this.toolbarInit(mainView, "Toolbar #2", "mydfa", "toolbar2", 275, 200, DSXDFPanel.dockLeft),
-            toolbar3: ToolbarPanel = this.toolbarInit(mainView, "Toolbar #3", "mydfa", "toolbar3", 350, 200, DSXDFPanel.dockRight),
-            toolbar4: ToolbarPanel = this.toolbarInit(mainView, "Toolbar #4", "mydfa", "toolbar4", 400, 100, DSXDFPanel.dockBottom);
+        this.toolbar1 = this.toolbarInit(this.mainView, "Toolbar #1", "mydfa", "toolbar1", 400, 100, DSXDFPanel.dockTop),
+        this.toolbar2 = this.toolbarInit(this.mainView, "Toolbar #2", "mydfa", "toolbar2", 275, 200, DSXDFPanel.dockLeft),
+        this.toolbar3 = this.toolbarInit(this.mainView, "Toolbar #3", "mydfa", "toolbar3", 350, 200, DSXDFPanel.dockRight),
+        this.toolbar4 = this.toolbarInit(this.mainView, "Toolbar #4", "mydfa", "toolbar4", 400, 100, DSXDFPanel.dockBottom);
 
-        toolbar1.setVisible(true);
-        toolbar2.setVisible(true);
-        toolbar3.setVisible(true);
-        toolbar4.setVisible(true);
-
-        console.log(toolbar1);
-        
-        /*This also works but it is inefficient: DON'T REPEAT YOURSELF!
-
-        var topToolbar: any = mainView.createDFPanel("Toolbar #1", "mydfa");
-        topToolbar.addContentDiv(document.getElementById("toolbar1"));
-        topToolbar.initLayout(300, 50, 400, 100, 1);
-
-        var leftToolbar: any = mainView.createDFPanel("Toolbar #2", "mydfa");
-        leftToolbar.addContentDiv(document.getElementById("toolbar2"));
-        leftToolbar.initLayout(300, 50, 250, 200, 0);
-
-        var rightToolbar: any = mainView.createDFPanel("Toolbar #3", "mydfa");
-        rightToolbar.addContentDiv(document.getElementById("toolbar3"));
-        rightToolbar.initLayout(300, 50, 250, 200, 2);
-
-        var bottomToolbar: any = mainView.createDFPanel("Toolbar #4", "mydfa");
-        bottomToolbar.addContentDiv(document.getElementById("toolbar4"));
-        bottomToolbar.initLayout(300, 50, 400, 100, 3);
-
-        */
-       
     }
 
     ngOnInit() {
