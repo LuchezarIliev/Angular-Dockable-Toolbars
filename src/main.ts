a047173f01153d5b83354a15a32afdef553b3e06
Georgi Danchev <Georgi Danchev@nemetschek.bg>
2018-08-07 14:07:55 +0300

    merge some more

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 5abc69f93f6926b12ba8523d540433378df40306
 if (environment.production) {
   enableProdMode();
 }
